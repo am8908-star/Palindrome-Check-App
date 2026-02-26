9c944e3affc54cef6ee49e567130494ef70c7ec1
am8908-star <am8908@srmist.edu.in>
2026-02-26 15:32:32 +0530

    UC6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,39 @@
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        String input = "refer";
+
+        String input = "madam";
+
+
+        Queue<Character> queue = new LinkedList<>();
 
         Stack<Character> stack = new Stack<>();
-
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            queue.add(ch);
+            stack.push(ch);
         }
 
-        String reversed = "";
+        boolean isPalindrome = true;
 
-        while (!stack.isEmpty()) {
-            reversed = reversed + stack.pop();
+        while (!queue.isEmpty()) {
+
+            char fromQueue = queue.remove();
+            char fromStack = stack.pop();
+
+            if (fromQueue != fromStack) {
+                isPalindrome = false;
+                break;
+            }
         }
 
-        System.out.println("Input String    : " + input);
-        System.out.println("Reversed String : " + reversed);
+        System.out.println("Input String : " + input);
 
-        if (input.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println("Result : The string is a Palindrome.");
         } else {
             System.out.println("Result : The string is NOT a Palindrome.");
